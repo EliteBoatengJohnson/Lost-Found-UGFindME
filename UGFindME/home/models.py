53b43a330fc76601d4f2638215108c4d13880ef1
Boateng Johnson <80701309+EliteBoatengJohnson@users.noreply.github.com>
2024-03-28 14:58:31 +0000

    update to the last commit

--- UGFindME/home/models.py
+++ UGFindME/home/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+#This is the schema for the various tables
+
+class Item(models.Model):
+    Name = models.CharField(max_length=70, blank=False)
+    discp = models.CharField(max_lenth = 10)
+    image = models.ImageField(help_text="Upload an image of item you Lost if possible")
+    doup = models.DateTimeField(auto_now= True)
